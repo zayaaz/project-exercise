2a1326d9fe7009d3721bd2aa66dc6a9a9067fcb0
zaya <zaya.erdenepil2017@gmail.com>
2021-12-09 00:30:18 -0500

    boilerplate update

---
+++ app/constants.php
@@ -0,0 +1,12 @@
+/**
+ * Constant variabless
+ */
+
+/** @const REPORT_KEY          A report key field to get study data. */
+const REPORT_KEY = [
+    'IO5'  => 'i05ClinobsValues',
+    'PA46' => 'i05ClinobsValues'
+];
+
+/** @const FILE_DATE_FORMAT    output file date format. */
+const FILE_DATE_FORMAT = 'Y-m-d-hms';

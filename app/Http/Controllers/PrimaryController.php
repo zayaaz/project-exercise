ef1e4523f5167d489380c1c8fcae18797138b397
zaya <zaya.erdenepil2017@gmail.com>
2021-12-09 00:45:42 -0500

    update

--- app/Http/Controllers/PrimaryController.php
+++ app/Http/Controllers/PrimaryController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use View;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Services\PrimaryServices;
 
 class PrimaryController extends Controller
 {
-    //
+    /** @var \App\Services\PrimaryServices $primaryServices   Primary service. */
+    protected $primaryServices;
+
+    /**
+     * Create a new PrimaryController.
+     *
+     */
+    public function __construct(PrimaryServices $primaryServices)
+    {
+        $this->primaryServices = $primaryServices;
+    }
+
+    /**
+     * Destroy this controller.
+     */
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    /**
+     * Display main landing page
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return view('welcome');
+    }
 }
